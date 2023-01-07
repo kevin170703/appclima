cac4c4126debbdc994d5c0380126050f16d0d440
kevin170703 <correakevinfabian01@gmail.com>
2023-01-07 10:35:21 -0300

    card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -16,33 +16,32 @@ export default function Card({
 }) {
   return (
     <div className="cardStyle">
-      <div className="contentButtonandTitle">
-        <NavLink to={`/ciudad/${id}`} className="title">
-          {name}
-        </NavLink>
-        <button onClick={onClose}>X</button>
-      </div>
-      <div>
-        <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} />
-        <div className="divMinMax">
-          <div className="temp">
-            <h5>{temp} °C</h5>
-          </div>
-          <div className="extraData">
-            <div className="parametros">
-              <p>Sensacion termica:</p>
-              <p>{feels_like} °C</p>
+      <NavLink to={`/ciudad/${id}`} className="link">
+        <div className="contentButtonandTitle">
+          <h4 className="title">{name}</h4>
+          <button onClick={onClose}>X</button>
+        </div>
+        <div>
+          <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} />
+          <div className="divMinMax">
+            <div className="temp">
+              <h5>{temp} °C</h5>
             </div>
-            <div className="parametros">
-              <p>Humedad:</p>
-              <p> {humidity} °C</p>
+            <div className="extraData">
+              <div className="parametros">
+                <p>Sensacion termica:</p>
+                <p>{feels_like} °C</p>
+              </div>
+              <div className="parametros">
+                <p>Humedad:</p>
+                <p> {humidity} °C</p>
+              </div>
+              <div className="parametros">
+                <p>Viento:</p>
+                <p>{wind_speed} km/h</p>
+              </div>
             </div>
-            <div className="parametros">
-              <p>Viento:</p>
-              <p>{wind_speed} km/h</p>
-            </div>
-          </div>
-          {/* <div className="divMin">
+            {/* <div className="divMin">
             <h5>Min</h5>
             <p>{min} °C</p>
           </div>
@@ -50,8 +49,9 @@ export default function Card({
             <h5>Max</h5>
             <p>{max} °C</p>
           </div> */}
+          </div>
         </div>
-      </div>
+      </NavLink>
     </div>
   );
 }
