ce20abf4c31175b700c84adf0b0bd20f4f3e5629
kevin170703 <correakevinfabian01@gmail.com>
2023-05-24 20:02:42 -0300

    Rediseñnado un poco

--- src/components/Ciudad.jsx
+++ src/components/Ciudad.jsx
@@ -7,13 +7,24 @@ export default function Ciudad({ city }) {
       <div className="container">
         <h2>{city.name}</h2>
         <div className="info">
-       
-          <div><b>Temperatura:</b>  {Math.round(city.temp) - 273} ºC</div>
-          <div><b>Clima:</b> {city.weather}</div>
-          <div><b>Viento:</b> {city.wind} km/h</div>
-          <div><b>Cantidad de nubes:</b> {city.clouds}</div>
-          <div><b>Latitud:</b> {city.latitud}º</div>
-          <div><b>Longitud:</b> {city.longitud}º</div>
+          <div className="contentData">
+            <b>Temperatura:</b> {Math.round(city.temp) - 273} ºC
+          </div>
+          <div className="contentData">
+            <b>Clima:</b> {city.weather}
+          </div>
+          <div className="contentData">
+            <b>Viento:</b> {city.wind} km/h
+          </div>
+          <div className="contentData">
+            <b>Cantidad de nubes:</b> {city.clouds}
+          </div>
+          <div className="contentData">
+            <b>Latitud:</b> {city.latitud}º
+          </div>
+          <div className="contentData">
+            <b>Longitud:</b> {city.longitud}º
+          </div>
         </div>
       </div>
     </div>
