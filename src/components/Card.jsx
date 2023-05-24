e87ed0b46a6cb532f734d84131f18f0ca836826f
kevin170703 <correakevinfabian01@gmail.com>
2023-05-24 20:02:12 -0300

    Rediseñnado un poco

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Card.css";
+import {
+  WiCloud,
+  WiHumidity,
+  WiStrongWind,
+  WiThermometer,
+} from "react-icons/wi";
+
+import { IoClose } from "react-icons/io5";
 
 export default function Card({
   temp,
@@ -16,41 +24,36 @@ export default function Card({
 }) {
   return (
     <div className="cardStyle">
-      <NavLink to={`/ciudad/${id}`} className="link">
-        <div className="contentButtonandTitle">
-          <h4 className="title">{name}</h4>
-          <button onClick={onClose}>X</button>
+      <div className="contentButtonandTitle">
+        <h4 className="title">{name}</h4>
+        <button onClick={onClose}>
+          <IoClose size="30" className="buttonClose" />
+        </button>
+      </div>
+
+      <div className="divMinMax">
+        <div className="temp">
+          <WiCloud size="120" />
+          <h5>{temp} °C</h5>
         </div>
-        <div>
-          <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} />
-          <div className="divMinMax">
-            <div className="temp">
-              <h5>{temp} °C</h5>
-            </div>
-            <div className="extraData">
-              <div className="parametros">
-                <p>Sensacion termica:</p>
-                <p>{feels_like} °C</p>
-              </div>
-              <div className="parametros">
-                <p>Humedad:</p>
-                <p> {humidity} °C</p>
-              </div>
-              <div className="parametros">
-                <p>Viento:</p>
-                <p>{wind_speed} km/h</p>
-              </div>
-            </div>
-            {/* <div className="divMin">
-            <h5>Min</h5>
-            <p>{min} °C</p>
+        <div className="extraData">
+          <div className="parametros">
+            <WiThermometer size="50" />
+            <p>{feels_like} °C</p>
           </div>
-          <div className="divMax">
-            <h5>Max</h5>
-            <p>{max} °C</p>
-          </div> */}
+          <div className="parametros">
+            <WiHumidity size="50" />
+            <p> {humidity} °C</p>
+          </div>
+          <div className="parametros">
+            <WiStrongWind size="50" />
+            <p>{wind_speed} km/h</p>
           </div>
         </div>
+      </div>
+
+      <NavLink to={`/ciudad/${id}`} className="moreDetails">
+        Mas detalles
       </NavLink>
     </div>
   );
